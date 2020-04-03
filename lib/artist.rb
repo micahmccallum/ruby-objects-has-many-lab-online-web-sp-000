9777850fedc2b526d3c2465abc6800c131799c59
Micah McCallum <micahmccallum@outlook.com>
2020-04-03 21:21:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,19 @@
 class Artist
-  attr_accessor :name, :song
+  attr_accessor :name
 
   def initialize(name)
     self.name = name
   end
 
-  # def songs
-  #   self.songs
-  # end
+  def add_song(song)
+    song.artist = self
+  end
+
+
+
+
+  def songs
+    Song.all.select {|song| song.artist == self }
+  end
 
 end
